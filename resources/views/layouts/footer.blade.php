1a96a7c0f6f7e410fb005259dbaaff53427484ec
Tetraxion <lutfid955@gmail.com>
2023-07-15 16:26:42 +0700

    memindahkan home

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,88 @@
+<footer class="footer section">
+    <div class="footer__container container grid">
+        <div>
+            <a href="{{ route('homepage') }}" class="footer__logo">
+                G<i class="bx bxs-map"></i> Gown
+            </a>
+            <p class="footer__description">
+                Our vision is to help people find the <br />
+                best places to gown with high security
+            </p>
+        </div>
+
+        <div class="footer__content">
+            <div>
+                <h3 class="footer__title">About</h3>
+
+                <ul class="footer__links">
+                    <li>
+                        <a href="#" class="footer__link">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link">Features </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link">News & Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="footer__title">Company</h3>
+
+                <ul class="footer__links">
+                    <li>
+                        <a href="#" class="footer__link">How We Work?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link">Capital </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link"> Security</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="footer__title">Support</h3>
+
+                <ul class="footer__links">
+                    <li>
+                        <a href="#" class="footer__link">FAQs </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link">Support center
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__link"> Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="footer__title">Follow us</h3>
+
+                <ul class="footer__social">
+                    <a href="#" class="footer__social-link">
+                        <i class="bx bxl-facebook-circle"></i>
+                    </a>
+                    <a href="#" class="footer__social-link">
+                        <i class="bx bxl-instagram-alt"></i>
+                    </a>
+                    <a href="#" class="footer__social-link">
+                        <i class="bx bxl-pinterest"></i>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer__info container">
+        <span class="footer__copy">
+            &#169; Kelompok 2
+        </span>
+        <div class="footer__privacy">
+            <a href="#">Terms & Agreements</a>
+            <a href="#">Privacy Policy</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    {{-- =============== BOXICONS =============== --}}
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+    {{-- =============== SWIPER CSS =============== --}}
+    <link rel="stylesheet" href="{{ asset('frontend/assets/libraries/swiper-bundle.min.css') }}" />
+
+    {{-- =============== CSS =============== --}}
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}" />
+    
+    @stack('style-alt')
+    <title>Gown Website</title>
+</head>
+
+<body>
+    {{-- ==================== HEADER ==================== --}}
+    @include('layouts.nav')
+
+    {{-- ==================== MAIN ==================== --}}
+    <main class="main">
+        @yield('content')
+    </main>
+
+    {{-- ==================== FOOTER ==================== --}}
+    @include('layouts.footer')
+
+    <!--========== SCROLL UP ==========-->
+    <a href="#" class="scrollup" id="scroll-up">
+        <i class="bx bx-chevrons-up"></i>
+    </a>
+
+    {{-- =============== SCROLLREVEAL =============== --}}
+    <script src="{{ asset('frontend/assets/libraries/scrollreveal.min.js') }}"></script>
+
+    <!--=============== SWIPER JS ===============-->
+    <script src="{{ asset('frontend/assets/libraries/swiper-bundle.min.js') }}"></script>
+
+    <!--=============== MAIN JS ===============-->
+    <script src="{{ asset('frontend/assets/js/main.js') }}"></script>
+    @stack('script-alt')
+</body>
+
+</html>
