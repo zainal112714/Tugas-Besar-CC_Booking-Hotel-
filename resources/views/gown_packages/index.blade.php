40121752dbdceeab2a2c48ea13552b653481ff29
Tetraxion <lutfid955@gmail.com>
2023-07-18 09:31:26 +0700

    perubahan sedikit pada keseluruhan

--- resources/views/gown_packages/index.blade.php
+++ resources/views/gown_packages/index.blade.php
@@ -38,7 +38,7 @@
                     />
                     <div class="popular__data">
                     <h2 class="popular__price"><span>$</span>{{ number_format($gown_package->price,2) }}</h2>
-                    <h3 class="popular__title">{{ $gown_package->location }}</h3>
+                    <h3 class="popular__title">{{ $gown_package->size }}</h3>
                     <p class="popular__description">{{ $gown_package->type }}</p>
                     </div>
                 </a>
